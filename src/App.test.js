673a8e442c97548997a7b322f4c7d5c59c244feb
David Chong <35475583+mootis@users.noreply.github.com>
2022-11-01 15:41:49 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/David Chong/i);
   expect(linkElement).toBeInTheDocument();
 });
